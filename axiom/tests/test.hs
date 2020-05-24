9e834e3f7d8c177d7e5a2ca160a72b1aaacda48c
agocorona <agocorona@gmail.com>
2020-05-24 20:37:04 +0000

    basic test for axiom

--- axiom/tests/test.hs
+++ axiom/tests/test.hs
@@ -1,16 +1,31 @@
 #!/usr/bin/env execthirdlinedocker.sh
 --  info: use sed -i 's/\r//g' file if report "/usr/bin/env: ‘execthirdlinedocker.sh\r’: No such file or directory"
--- LIB="~/workspace/transient-stack" && mkdir -p static && ghcjs --make   -i${LIB}/transient/src -i${LIB}/transient-universe/src  -i${LIB}/axiom/src   $1 -o static/out && runghc   -DDEBUG  -i${LIB}/transient/src -i${LIB}/transient-universe/src -i${LIB}/axiom/src   $1 ${2} ${3}
-
-
+-- LIB="/projects/transient-stack" && mkdir -p static && ghcjs -DDEBUG  --make   -i${LIB}/transient/src -i${LIB}/transient-universe/src  -i${LIB}/axiom/src   $1 -o static/out && runghc   -DDEBUG  -i${LIB}/transient/src -i${LIB}/transient-universe/src -i${LIB}/axiom/src   $1 ${2} ${3}
+{-# LANGUAGE CPP #-}
 import GHCJS.HPlay.View
 import Control.Applicative
 import Transient.Base
-import Transient.Move
-
-main =  keep $ initNode $ action
+import Transient.Move.Internals
+import Control.Monad.IO.Class
+#ifndef ghcjs_HOST_OS
+import qualified Network.WebSockets.Connection          as WS
+import Data.IORef
+import qualified Data.ByteString.Lazy.Char8             as BS
+#endif
+main =  keep $ initNode action
 
 action :: Cloud ()
-action = local $ do
-  r <- render $ wbutton 10 (toJSString "try this")
-  render $ wraw (h1 (show r))
+action =   do
+  local $ setRState (0::Int)
+  onAll $ liftIO $ print ("ACTION" ::String)
+  local $ render $ wbutton "hello" (toJSString "try this")
+
+  r <- local $ getRState <|> return 0
+  local $ setRState $ r + 1
+  atRemote $ local $ do
+      liftIO $ print r 
+
+  local $ render $ wraw (h1 (r :: Int))
+
+
+
