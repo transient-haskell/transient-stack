87098b81d5a4dc49f382dba1f3c451d9ba81f2d3
agocorona <agocorona@gmail.com>
2024-06-13 09:33:45 +0000

    Tests (not finished, fails)

--- transient-universe/tests/TestSuite.hs
+++ transient-universe/tests/TestSuite.hs
@@ -9,6 +9,7 @@ import           Control.Applicative
 import           Data.Monoid
 import           Transient.Base
 import           Transient.Internals
+import           Transient.Console
 import           Transient.Indeterminism
 import           Transient.Move.Internals
 import           Transient.Move.Utils
@@ -19,6 +20,7 @@ import qualified Data.Map as M
 import           System.Exit
 import           Control.Monad.State
 import           Control.Exception
+import           Control.Concurrent.MVar
 
 import           Control.Concurrent(threadDelay )
 
@@ -35,6 +37,9 @@ service= Service $ M.fromList
          ,("executable", "test-transient1")
          ,("package","https://github.com/agocorona/transient-universe")]
 
+
+
+
 main= do
      mr <- keep test
      endMonitor 
@@ -48,57 +53,93 @@ main= do
 
 portnumber= 8081
 
+liftA1 tcomp ccomp= local $ tcomp $ runCloud ccomp
+
 test=  initNodeServ service  "localhost" portnumber $ do
+
+    -- ( thereIsArgPath' >> interactive >>= testIt)  <|> 
+    (batchTest >>= testIt >> exitIt )
+
+
+
+thereIsArgPath'= local $ Transient $ liftIO $ do
+  ph <- thereIsArgPath
+  if null ph then return Nothing else return $ Just ph
+
+interactive= do
+    liftA1 fork  inputNodes
+    local $ sync $ ( option "f" "fire")
+    local $ do ns <- getNodes
+               return $ tail ns
+
+    
+exitIt= onAll $ exit (Nothing  :: Maybe SomeException)
+
+batchTest= do
+          localIO $ print "batchtest"
           node0 <- local getMyNode
           local $ guard (nodePort node0== portnumber)       -- only executes locally in node "portnumber"
-          
-          [node1] <-  requestInstance service 1
-          -- onAll $ liftIO $ print (node1,node2)
-          
-          local ( option "f" "fire")   <|> return ""       -- to repeat the tests using the option fire,  remove the "exit" at the end 
+          localIO $ print "REQEST"
+          requestInstance service 3
 
+
+
+testIt nodes = do
+          let node1:node2:node3:_ = nodes
+          node0 <- local getMyNode
+          -- onAll $ liftIO $ print (node1)
       --     onAll $ ttr ("node1",node1,"node2",node2)
 
-          -- localIO $ putStrLn "------checking  empty in remote node when the remote call back to the caller #46 --------"
+          localIO $ putStrLn "------checking  empty in remote node when the remote call back to the caller #46 --------"
           
-          -- r <- runAt node1 $ do
-          --      SHOULDRUNIN(node1)
-          --      runAt node2 $  (runAt node1 $ SHOULDRUNIN(node1) >> empty ) <|>  (SHOULDRUNIN(node2) >> return "world")
-          -- localIO $ print r
+          -- runAt node1 $ runAt node2 $ runAt node1 $ return "hello"
+          -- empty
+
+          r <- runAt node1 $ do
+                   runAt node2 $  (runAt node1 $ SHOULDRUNIN(node1) >>  return "hello" ) 
+                   
+          localIO $ putStrLn r
+
+          empty
           
 
-          localIO $ putStrLn "------checking Alternative distributed--------"
+          -- localIO $ putStrLn "------checking Alternative distributed--------"
 
 
-          -- r <- local $ collect 2 $  runCloud $ (runAt node0 (SHOULDRUNIN( node0) >> return "hello" )) <|>
-          --       (runAt node1 (SHOULDRUNIN( node1) >> return "world" )) -- <|>
+          -- r <-  (runAt node0 (SHOULDRUNIN( node0) >> return "hello" )) <|>
+          --        (runAt node1 (SHOULDRUNIN( node1) >> return "world" )) -- <|>
           --       -- (runAt node2 (SHOULDRUNIN( node2) >> return "world2" ))
+          -- -- 
           
+          -- -- assert(sort r== ["hello", "world"]) $ localIO $  print r
+
+          -- -- r <- runAt node1 $ local $ return "world "
+          -- localIO $ print r
+          -- empty
+
           
-          -- assert(sort r== ["hello", "world"]) $ localIO $  print r         
-          
-          localIO $ putStrLn "--------------checking Applicative distributed--------"
-          r <- loggedc $(runAt node0 (SHOULDRUNIN( node0) >> return "hello "))
-                    <>  (runAt node1 (SHOULDRUNIN( node1) >> return "world " ))
-                    -- <>  (runAt node2 (SHOULDRUNIN( node2) >> return "world2" ))
+          -- localIO $ putStrLn "--------------checking Applicative distributed--------"
+          -- r <- loggedc $(runAt node0 (SHOULDRUNIN( node0) >> return "hello "))
+          --           <>  (runAt node1 (SHOULDRUNIN( node1) >> return "world " ))
+          --           -- <>  (runAt node2 (SHOULDRUNIN( node2) >> return "world2" ))
+       
+          -- assert(r== "hello world ") $ localIO $ print r
 
-          assert(r== "hello world world2") $ localIO $ print r
+          localIO $ putStrLn "----------------checking monadic, distributed-------------"
+          r <- runAt node0 (SHOULDRUNIN(node0)
+                  >> runAt node1 (SHOULDRUNIN(node1) >>  (return "HELLO" )))
+                      --  >> runAt node2 (SHOULDRUNIN(node2) >>  (return "HELLO" ))))
 
-          -- localIO $ putStrLn "----------------checking monadic, distributed-------------"
-          -- r <- runAt node0 (SHOULDRUNIN(node0)
-          --         >> runAt node1 (SHOULDRUNIN(node1)
-          --              >> runAt node2 (SHOULDRUNIN(node2) >>  (return "HELLO" ))))
-
-          -- assert(r== "HELLO") $ localIO $ print r
+          assert(r== "HELLO") $ localIO $ print r
  
-          -- localIO $ putStrLn "----------------checking map-reduce -------------"
+          -- -- localIO $ putStrLn "----------------checking map-reduce -------------"
 
           -- r <- reduce  (+)  . mapKeyB (\w -> (w, 1 :: Int))  $ getText  words "hello world hello"
           -- localIO $  print r
           -- assert (sort (M.toList r) == sort [("hello",2::Int),("world",1)]) $ return r
           
           return  (Nothing  :: Maybe SomeException) 
-          -- onAll $ exit (Nothing  :: Maybe SomeException) -- remove this to repeat the test
+
           
  
 
