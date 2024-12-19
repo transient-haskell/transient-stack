965cce46ec1bff57e99ce9327f883f628f3056ef
agocorona <agocorona@gmail.com>
2024-12-19 23:22:29 +0000

    added cabal.project

--- transient/tests/TestSuite.hs
+++ transient/tests/TestSuite.hs
@@ -36,7 +36,7 @@ main = do
 
     void $ collect 0 $ do
       -- gather the result of 100 iterations
-      i <- threads 0 $ choose [1 .. 1000] -- test 100 times. 'loop' for 100 times
+      i <- threads 0 $ choose [1 .. 100] -- test 100 times. 'loop' for 100 times
       nelems <- liftIO $ randomRIO (1, 100) -- :: TransIO Int
       nthreads <- liftIO $ randomRIO (0, nelems) -- different numbers of threads
       r <- threads nthreads $ sum $ map genElem [1 .. nelems] -- sum sync and async results using applicative
@@ -46,7 +46,6 @@ main = do
     liftIO $ putStrLn "--------------checking  parallel execution, Alternative, events, collect --------"
     ev <- newEVar
     r <- collect 300 $ readEVar ev <|> ((choose [1 .. 300] >>= writeEVar ev) >> stop)
-    liftIO $ print r
     assert (sort r == [1 .. 300]) $ return ()
 
     liftIO $ print "SUCCESS"
